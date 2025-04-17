2f2ce9b53a4816e8dd0dd14e092fc960f3d25300
chirathdinduwara <tdchirathinduwara@gmail.com>
2025-04-17 10:05:28 +0530

    initial commit

---
+++ backend/learngram/src/main/java/com/learngram/learngram/LearngramApplication.java
@@ -0,0 +1,13 @@
+package com.learngram.learngram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearngramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearngramApplication.class, args);
+	}
+
+}
---
+++ backend/learngram/src/test/java/com/learngram/learngram/LearngramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learngram.learngram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearngramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
