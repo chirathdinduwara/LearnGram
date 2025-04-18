86d12e3f438167f959b0bc7ced34aa17a6b4aa1c
Lakshan03X <nipunlakshan9255@gmail.com>
2025-04-18 10:04:11 +0530

    mini bug fixed

--- frontend/src/components/Story/StoryIcon.jsx
+++ frontend/src/components/Story/StoryIcon.jsx
@@ -1,21 +1,18 @@
-
 function StoryIcon() {
-    return(
-        <>
-<div className="story-icon">
-  <div className="story-img">
-    <img
-      src="https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3383.jpg"
-      alt="Default Avatar"
-      className="story-profile"
-    />
-  </div>
-  <p className="story-name">User NAME</p>
-</div>
-
-
-        </>
-    );
+  return (
+    <>
+      <div className="story-icon">
+        <div className="story-img">
+          <img
+            src="https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3383.jpg"
+            alt="Default Avatar"
+            className="story-profile"
+          />
+        </div>
+        <p className="story-name">Apilage Sadu</p>
+      </div>
+    </>
+  );
 }
 
 export default StoryIcon;
--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,20 +1,19 @@
 import NavBar from "../../components/Home/NavBar";
 import StoryBar from "../../components/Home/StoryBar";
 
-import '../../css/Home/HomePage.css';
-
+import "../../css/Home/HomePage.css";
 
 function HomePage() {
-    return(
-        <>
-            <div className="home-container">
-                <NavBar />
-                <div className="home-contents">
-                    <StoryBar />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="home-container">
+        <NavBar />
+        <div className="home-contents">
+          <StoryBar />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
