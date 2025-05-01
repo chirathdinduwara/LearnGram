728e144ba17550b70c85638ef34dee632da3a341
chirathdinduwara <tdchirathinduwara@gmail.com>
2025-05-01 12:55:50 +0530

    conflict resolved

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-function Post({ img, profile, caption, location, name }) {
-  return (
-    <>
-      <div className="post">
-        <div className="post-header">
-          <img
-            src={profile}
-            alt="Default Avatar"
-            className="story-profile"
-            style={{ width: "40px" }}
-          />
-          <div className="post-header-details">
-            <div className="post-header-details-left">
-              <p className="post-u-name">{name}</p>
-              <p className="post-location">{location}</p>
-=======
+
 function Post({img, profile, caption, location, name, onClick}) {
 
 
@@ -45,23 +29,8 @@ function Post({img, profile, caption, location, name, onClick}) {
                         <p>{caption}</p>
                     </div>
                 </div>
->>>>>>> main
-            </div>
-            <div className="post-header-details-right">
-              <p className="post-time"> • 3d</p>
-            </div>
-          </div>
-        </div>
-        <div className="post-content">
-          <img className="post-img" src={img} alt="" srcset="" />
-          <div className="post-caption">
-            <p className="post-u-name">{name}</p>
-            <p>{caption}</p>
-          </div>
-        </div>
       </div>
     </>
-  );
-}
+  )};
 
 export default Post;
