4c20f97784f009e9c2cfce4112bd8be59924cc28
R-abhisheka <rashmiabhisheka7426@gmail.com>
2025-05-12 14:05:13 +0530

    fix SToryService.java

--- backend/learngram/src/main/java/com/learngram/learngram/services/StoryService.java
+++ backend/learngram/src/main/java/com/learngram/learngram/services/StoryService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import com.learngram.learngram.domain.Story;
 
-public interface StoryService {
+public interface StoryServic {
 
     Story createStory(Story story);
 
